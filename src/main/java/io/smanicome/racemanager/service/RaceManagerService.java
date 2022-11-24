f764aaa9a12ad0f44858841be58652d2a844b282
Saravanane MANICOME <saravanane.manicome@carbon-it.com>
2022-11-24 17:37:52 +0100

    feat: update service interface to use exceptions

--- src/main/java/io/smanicome/racemanager/service/RaceManagerService.java
+++ src/main/java/io/smanicome/racemanager/service/RaceManagerService.java
@@ -1,7 +1,9 @@
 package io.smanicome.racemanager.service;
 
 import io.smanicome.racemanager.core.Race;
+import io.smanicome.racemanager.exceptions.RaceNumberAlreadyUsedForRequestedDateException;
+import io.smanicome.racemanager.exceptions.RunnerNumberBreakingSequenceException;
 
 public interface RaceManagerService {
-    Race createRace(Race race);
+    Race createRace(Race race) throws RaceNumberAlreadyUsedForRequestedDateException, RunnerNumberBreakingSequenceException;
 }
