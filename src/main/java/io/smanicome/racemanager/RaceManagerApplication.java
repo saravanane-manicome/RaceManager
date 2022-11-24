129976968ce907acdbfa4c67dbfc074104f0c1f7
Saravanane MANICOME <saravanane.manicome@carbon-it.com>
2022-11-24 13:56:20 +0100

    first commit

---
+++ src/main/java/io/smanicome/racemanager/RaceManagerApplication.java
@@ -0,0 +1,13 @@
+package io.smanicome.racemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaceManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/smanicome/racemanager/RaceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.smanicome.racemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
