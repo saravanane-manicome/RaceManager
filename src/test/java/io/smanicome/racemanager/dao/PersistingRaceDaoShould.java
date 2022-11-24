dffef61729e486f7795431ac8a527c55a978ead5
Saravanane MANICOME <saravanane.manicome@carbon-it.com>
2022-11-24 15:36:20 +0100

    test: assert that runners are saved along race

--- src/test/java/io/smanicome/racemanager/dao/PersistingRaceDaoShould.java
+++ src/test/java/io/smanicome/racemanager/dao/PersistingRaceDaoShould.java
@@ -1,6 +1,7 @@
 package io.smanicome.racemanager.dao;
 
 import io.smanicome.racemanager.repository.JpaRaceEntity;
+import io.smanicome.racemanager.repository.JpaRunnerEntity;
 import io.smanicome.racemanager.repository.RaceRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,20 +29,43 @@ class PersistingRaceDaoShould {
     @Test
     @DisplayName("save a race")
     void shouldSaveRace() {
-        final var id = UUID.randomUUID();
-        final var date = LocalDateTime.now();
+        final var raceId = UUID.randomUUID();
+        final var runnerId1 = UUID.randomUUID();
+        final var runnerId2 = UUID.randomUUID();
+        final var runnerId3 = UUID.randomUUID();
 
-        final var raceEntityToSave = new RaceEntity(null, date, 0, List.of());
-        final var jpaRaceEntity = new JpaRaceEntity(id, date, raceEntityToSave.number(), List.of());
+        final var runnerEntitiesToSave = List.of(
+            new RunnerEntity(null, "runner1", 1),
+            new RunnerEntity(null, "runner2", 2),
+            new RunnerEntity(null, "runner3", 3)
+        );
+        final var raceEntityToSave = new RaceEntity(null, LocalDateTime.now(), 0, runnerEntitiesToSave);
+
+        final var jpaRunnerEntities = List.of(
+                new JpaRunnerEntity(runnerId1, "runner1", 1, null),
+                new JpaRunnerEntity(runnerId2, "runner2", 2, null),
+                new JpaRunnerEntity(runnerId3, "runner3", 3, null)
+        );
+        final var jpaRaceEntity = new JpaRaceEntity(raceId, raceEntityToSave.date(), raceEntityToSave.number(), jpaRunnerEntities);
 
         when(raceRepository.save(any())).thenReturn(jpaRaceEntity);
 
         final var savedRaceEntity = persistingRaceDao.save(raceEntityToSave);
 
-        final var expectedRaceEntity = new RaceEntity(id, date, 0, List.of());
+        final var expectedRunnerEntities = List.of(
+                new RunnerEntity(runnerId1, "runner1", 1),
+                new RunnerEntity(runnerId2, "runner2", 2),
+                new RunnerEntity(runnerId3, "runner3", 3)
+        );
+        final var expectedRaceEntity = new RaceEntity(raceId, raceEntityToSave.date(), 0, expectedRunnerEntities);
         assertEquals(expectedRaceEntity, savedRaceEntity);
 
-        verify(raceRepository).save(new JpaRaceEntity(null, date, raceEntityToSave.number(), List.of()));
+        final var jpaRunnerEntitiesToSave = List.of(
+                new JpaRunnerEntity(null, "runner1", 1, null),
+                new JpaRunnerEntity(null, "runner2", 2, null),
+                new JpaRunnerEntity(null, "runner3", 3, null)
+        );
+        verify(raceRepository).save(new JpaRaceEntity(null, raceEntityToSave.date(), raceEntityToSave.number(), jpaRunnerEntitiesToSave));
         verifyNoMoreInteractions(raceRepository);
     }
 }
