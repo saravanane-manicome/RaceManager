4391b0a16884643a917da69597e8447b72613936
Saravanane MANICOME <saravanane.manicome@carbon-it.com>
2022-11-25 17:37:45 +0100

    feat: enable jpa repositories

---
+++ src/main/java/io/smanicome/racemanager/repository/DataConfiguration.java
@@ -0,0 +1,7 @@
+package io.smanicome.racemanager.repository;
+
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories
+public class DataConfiguration {
+}
